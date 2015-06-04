245694952b87a64d852ff1d8a6e0987201a3186f
aman086 <aman086@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-04 19:25:19 +0000

    fixed warning
    git-svn-id: https://plugins.svn.wordpress.org/ninja-forms-recaptcha-field/trunk@1174687 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ninja-forms-recaptcha.php
+++ ninja-forms-recaptcha.php
@@ -5,9 +5,9 @@ Description: Adds reCAPTCHA field to Ninja Forms.
 Author: Aman Saini
 Author URI: http://amansaini.me
 Plugin URI: http://amansaini.me
-Version: 1.0
+Version: 1.1
 Requires at least: 3.5
-Tested up to: 4.1
+Tested up to: 4.2
 
  */
 
@@ -39,28 +39,28 @@ class Ninja_Forms_Recaptcha_Field {
 
 
 		//Register the Upload field
-		add_action( 'init', array($this,'recaptcha_field_register' ));
+		add_action( 'init', array( $this, 'recaptcha_field_register' ) );
 
 		// Add Recaptcha Settings submenu under Forms
-			add_action( 'admin_menu', array( $this, 'register_recaptcha_submenu_page' ) );
+		add_action( 'admin_menu', array( $this, 'register_recaptcha_submenu_page' ) );
 
-			add_action( 'admin_init', array( $this, 'initialize_recaptcha_options' ) );
+		add_action( 'admin_init', array( $this, 'initialize_recaptcha_options' ) );
 
 	}
 	function recaptcha_field_register() {
 		$args = array(
 			'name' => 'reCAPTCHA',
 			// 'edit_options' => array(
-			// 	array(
-			// 		'type' => 'text',
-			// 		'name' => 'recaptcha_field_error',
-			// 		'label' => 'Error message on wrong Captcha',
-			// 		'class' => 'widefat',
-			// 		'value' =>'Captcha mismatch, Please try again.'
-			// 	),
+			//  array(
+			//   'type' => 'text',
+			//   'name' => 'recaptcha_field_error',
+			//   'label' => 'Error message on wrong Captcha',
+			//   'class' => 'widefat',
+			//   'value' =>'Captcha mismatch, Please try again.'
+			//  ),
 
 			// ),
-			'display_function' => array($this,'ninja_forms_recaptcha_display'),
+			'display_function' => array( $this, 'ninja_forms_recaptcha_display' ),
 			'group' => '',
 			'edit_label' => true,
 			'req' => true,
@@ -71,8 +71,8 @@ class Ninja_Forms_Recaptcha_Field {
 			'edit_meta' => false,
 			'sidebar' => 'template_fields',
 			'edit_conditional' => false,
-			'pre_process' => array($this,'ninja_forms_field_recaptcha_pre_process'),
-			'req_validation' => array($this, 'ninja_forms_field_recaptcha_req_validation'),
+			'pre_process' => array( $this, 'ninja_forms_field_recaptcha_pre_process' ),
+			//'req_validation' => array( $this, 'ninja_forms_field_recaptcha_req_validation' ),
 		);
 		ninja_forms_register_field( 'g_recaptcha', $args );
 
@@ -80,59 +80,57 @@ class Ninja_Forms_Recaptcha_Field {
 
 	}
 
-	public function ninja_forms_recaptcha_display(){
+	public function ninja_forms_recaptcha_display() {
 		$lang = 'en';
-		$settings = get_option('nf_recaptcha_settings');
+		$settings = get_option( 'nf_recaptcha_settings' );
 
-		 $siteKey = $settings['site_key'];
-		  if( !empty($siteKey)){
-
-		?>
+		$siteKey = $settings['site_key'];
+		if ( !empty( $siteKey ) ) { ?>
 		<div class="g-recaptcha" data-sitekey="<?php echo $siteKey; ?>"></div>
             <script type="text/javascript" src="https://www.google.com/recaptcha/api.js?hl=<?php echo $lang; ?>">
             </script>
 
 		<?php
-	}else{
-		echo 'Please enter Site key for Google reCAPTCHA';
-	}
+		}else {
+			echo 'Please enter Site key for Google reCAPTCHA';
+		}
 	}
 
-	function ninja_forms_field_recaptcha_pre_process(){
+	function ninja_forms_field_recaptcha_pre_process() {
 
 		global $ninja_forms_processing;
 
-		if( !isset($_POST['g-recaptcha-response'])){
+		if ( !isset( $_POST['g-recaptcha-response'] ) ) {
 			return;
 		}
 
-		$settings = get_option('nf_recaptcha_settings');
+		$settings = get_option( 'nf_recaptcha_settings' );
 
 		$url = 'https://www.google.com/recaptcha/api/siteverify?secret='.$settings['secret_key'].'&response='.$_POST['g-recaptcha-response'];
 
-		$resp = wp_remote_get( $url,array('sslverify'=>true ));
+		$resp = wp_remote_get( $url, array( 'sslverify'=>true ) );
 
-		if(!is_wp_error( $resp )){
+		if ( !is_wp_error( $resp ) ) {
 
 			$body = wp_remote_retrieve_body( $resp );
 
-			$response = json_decode($body);
+			$response = json_decode( $body );
 
-			if($response->success===false){
+			if ( $response->success===false ) {
 
-				if( !empty($response->{'error-codes'}) && $response->{'error-codes'}!='missing-input-response'){
+				if ( !empty( $response->{'error-codes'} ) && $response->{'error-codes'}!='missing-input-response' ) {
 
-						$error= 'Please check if you have entered Site & Secret key correctly';
+					$error= 'Please check if you have entered Site & Secret key correctly';
 
-					}else{
+				}else {
 
-						$error= 'Captcha mismatch, Please try again.';
-					}
-
-					 $ninja_forms_processing->add_error( 'error_recaptcha', $error);
+					$error= 'Captcha mismatch, Please try again.';
 				}
 
+				$ninja_forms_processing->add_error( 'error_recaptcha', $error );
 			}
+
+		}
 
 	}
 
@@ -141,7 +139,7 @@ class Ninja_Forms_Recaptcha_Field {
 		add_submenu_page( 'ninja-forms', 'reCAPTCHA Settings', 'reCAPTCHA', 'manage_options', 'nf_recaptcha_settings', array( $this, 'nf_recaptcha_settings_callback' ) );
 	}
 
-		function nf_recaptcha_settings_callback() {
+	function nf_recaptcha_settings_callback() {
 ?>
 		<!-- Create a header in the default WordPress 'wrap' container -->
     <div class="wrap">
@@ -234,7 +232,7 @@ class Ninja_Forms_Recaptcha_Field {
 	}
 
 
-		/*
+	/*
 	 * Check if Ninja form is  installed
 	 */
 	private static function is_ninjaform_installed() {
@@ -242,6 +240,3 @@ class Ninja_Forms_Recaptcha_Field {
 	}
 
 }
-
-
-
--- readme.txt
+++ readme.txt
@@ -4,35 +4,19 @@ Tags: ninja forms, forms, field, google recaptcha
 Donate link: http://amansaini.me
 Requires at least: 3.4
 Tested up to: 4.2
-Stable tag: 1.0
+Stable tag: 1.1
 License: GPLv2 or later
 
  Adds reCAPTCHA field to Ninja Forms.
 
 == Description ==
-
-`Tough on bots, Easy on humans`
-
-
-Adds a **Google reCAPTCHA** field type to Ninja Forms form builder.
-Forms with a **reCAPTCHA** will validate the field before successful submission.
-
-`How to use it?`
-
-* Upload the plugin to wp-content->plugins directory,
-* Activate it from wordpress dashboard,
-* Add the Site Key and Secret on Forms->reCaptcha,
-* Edit Form and you will see Google reCaptcha field under Template Fields
-
-`Have Suggestion/Feedback`
-
- Please send me your feedback  <a href="http://amansaini.me/contact/">here</a> for any new features you want to see in next version of this plugin.I will be glad to receive feedback.
+Adds a "No CAPTCHA reCAPTCHA" field type to Ninja Forms form builder Forms with a "No CAPTCHA reCAPTCHA" will validate the field before successful submission.
 
 == Installation ==
-* Upload the plugin to wp-content->plugins directory,
-* Activate it from wordpress dashboard,
-* Add the Site Key and Secret on Forms->reCaptcha,
-* Edit Form and you will see Google reCaptcha field under Template Fields
+* Upload the plugin to wp-content->plugins directory
+* Activate it from wordpress dashboard
+* Add the Site Key and Secret from Forms->ReCaptcha
+* Edit Form and you will see Google ReCaptcha field under Template Fields
 
 == Screenshots ==
 
@@ -40,6 +24,9 @@ Forms with a **reCAPTCHA** will validate the field before successful submission.
 2. reCAPTCHA field
 
 == Changelog ==
+
+= 1.1 =
+Fixed warning message.
 
 = 1.0 =
 Intial Release
